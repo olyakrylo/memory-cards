e561105528fd446c216fcbc06a5f24d7cdd774ca
olyakrylo <olyakrylo24@gmail.com>
2020-08-18 08:13:57 +0300

    add build

---
+++ docs/precache-manifest.ef8811538b9837872395ba75658efb39.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f524058fe3610c9bb6993485c498a0c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e0a15de12f39527226ed",
+    "url": "./static/css/main.73a150ba.chunk.css"
+  },
+  {
+    "revision": "447c43373176b6cbf2ef",
+    "url": "./static/js/2.f0ffb66b.chunk.js"
+  },
+  {
+    "revision": "643efbae9d6e0fbba269bfc7ee232ed1",
+    "url": "./static/js/2.f0ffb66b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0a15de12f39527226ed",
+    "url": "./static/js/main.3537150e.chunk.js"
+  },
+  {
+    "revision": "2430d041e37990463684",
+    "url": "./static/js/runtime-main.789bd95f.js"
+  },
+  {
+    "revision": "a5025c585afba85d5fc35d933625c319",
+    "url": "./static/media/NotoSans-Bold.a5025c58.woff"
+  },
+  {
+    "revision": "10ad0f861c0c5807734017c341940649",
+    "url": "./static/media/Roboto-Light.10ad0f86.woff"
+  },
+  {
+    "revision": "94dac78eee406a8c8f0406b69b85ac2b",
+    "url": "./static/media/Roboto-Regular.94dac78e.woff"
+  }
+]);
